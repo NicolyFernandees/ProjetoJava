1fd800f24fe82ed353ae20a3d5013e10342ddc4c
NicolyFernandees <nycolysennaa@gmail.com>
2025-03-15 15:41:37 -0300

    Dados não primitivos entrada e saída;

---
+++ ProjetoJavaNaruto/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        String ninja1 = "Sasuke Uchiha";
+        int idade1 = 17;
+
+
+        System.out.println("Nome do ninja: " + ninja1);
+        System.out.println("Idade do ninja: " + idade1);
+
+
+        }
+}
